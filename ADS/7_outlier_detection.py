ba9f52b5150a0ba6e148202e8aec709f10c6e4f4
Shyren <shyren.more30@gmail.com>
2023-04-22 20:36:34 +0530

    add outlier detection: k-neighbors

---
+++ ADS/7_outlier_detection.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+df = load_iris()
+print(df)
+X = df.data
+y = df.target
+
+
+from sklearn.neighbors import NearestNeighbors
+model = NearestNeighbors(n_neighbors=3)
+model.fit(X, y)
+
+# distances and indexes of k-neaighbors from model outputs
+distances, indexes = model.kneighbors(X)
+# plot mean of k-distances of each observation
+plt.plot(distances.mean(axis=1))
+plt.show()
+
+# cut outlier values of >0.15
+outlier_idx = np.where(distances.mean(axis=1) > 0.15)
+# filter outlier values
+outlier_attributes = df.data[outlier_idx]
+print(outlier_attributes)
